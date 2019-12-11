5933cc43e6877653f7dab5cc56c37a9888251e93
René Moret <rpemoret@hotmail.com>
2019-12-11 20:44:23 +0100

    Create debuggen.js

---
+++ debuggen.js
@@ -0,0 +1,18 @@
+// debuggen
+
+const array = [
+  { name: "N. Armstrong", profession: "spacecowboy", age: 89 },
+  { name: "H. de Haan", profession: "kippen hypnotiseur", age: 59 },
+  { name: "A. Curry", profession: "kikvorsman", age: 32 },
+  { name: "F. Vonk", profession: "slangenmelker", age: 36 },
+  { name: "B. Bunny", profession: "konijnen uitlaatservice", age: 27 },
+  { name: "Dr.Evil", profession: "digital overlord", age: 56 }
+];
+
+for (let person of array) {
+  console.log(person);
+
+  console.log("Dit is " + person.name + ", " + person.age);
+
+  console.log(person.name + " is a " + person.profession);
+}
