8268dfbb7871a8e15bfb2611cdaa35d33fde0e75
René Moret <rpemoret@hotmail.com>
2019-12-19 12:45:01 +0100

    apiintegration

--- api-client2.js
+++ api-client2.js
@@ -3,5 +3,31 @@
 const datafromAPI = async function() {
   const data = await getData();
   console.log("Here's the data from the API", data);
+
+  addlisttoDOM = function(setdata) {
+    const list = document.getElementById("list");
+    setdata.forEach(function(data) {
+      let listitem = document.createElement("li");
+
+      listitem.src = data;
+      let contentoflistitem = document.createTextNode(data);
+    });
+    list.appendChild(listitem);
+    listitem.appendChild(contentoflistitem);
+  };
+
+  addlisttoDOM();
+
+  console.log(data);
 };
 datafromAPI();
+
+const listelement = function() {
+  createElement("p");
+};
+listelement();
+console.log("p");
+
+document.addEventListener("DOMContentLoaded", event => {
+  addlisttoDOM();
+});
